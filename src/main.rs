567fe14c255ff62718edef507080bf0110d2eef2
Matt Gabrenya <matt@buildyourweb.app>
2023-10-10 15:18:29 -0700

    chore: fmt + clippy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
 use lair_keystore_api::dependencies::sodoken::BufRead;
 use lair_keystore_api::ipc_keystore_connect;
-use tokio;
-use url::Url;
 
+use url::Url;
 
 /// Verify a holochain conductor is running and has a reachable app websocket at a given URL
 #[derive(Parser, Debug)]
@@ -20,16 +19,17 @@ struct Args {
 async fn main() {
     let args = Args::parse();
 
-    let socket_url_parsed = Url::parse(args.socket_url.clone().as_str())
-        .expect(format!("socket_url {:?} is not a valid URL", args.socket_url.clone()).as_str());
-    let password_bufread: BufRead = args.password
-        .as_bytes()
-        .into();
+    let socket_url_parsed = Url::parse(args.socket_url.clone().as_str()).unwrap_or_else(|_| panic!("socket_url {:?} is not a valid URL",
+            args.socket_url.clone()));
+    let password_bufread: BufRead = args.password.as_bytes().into();
 
     let _lair_client = ipc_keystore_connect(socket_url_parsed, password_bufread)
         .await
         .map_err(|e| format!("Failed to connect to lair keystore {:?}", e))
         .unwrap();
 
-    println!("Successfully connected to lair keystore at {:?}", args.socket_url);
+    println!(
+        "Successfully connected to lair keystore at {:?}",
+        args.socket_url
+    );
 }
