7eb989acf02bc55669cdb2d9d4493f8086a22550
Matt Gabrenya <matt@buildyourweb.app>
2023-10-10 15:16:00 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Holochain App WS Tester
+
+Verify a holochain conductor is running and has a reachable app websocket at a given URL
+
+## Usage
+```
+holochain-app-ws-tester [OPTIONS] <APP_WS>
+
+Arguments:
+  <APP_WS>  Holochain App WS URL
+
+Options:
+  -i, --app-id <APP_ID>  hApp app-id running on the conductor
+  -h, --help             Print help
+  -V, --version          Print version
+```
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use clap::Parser;
+use lair_keystore_api::dependencies::sodoken::BufRead;
+use lair_keystore_api::ipc_keystore_connect;
+use tokio;
+use url::Url;
+
+
+/// Verify a holochain conductor is running and has a reachable app websocket at a given URL
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Lair Keystore Socket URL
+    socket_url: String,
+
+    /// Lair Keystore Password
+    password: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let socket_url_parsed = Url::parse(args.socket_url.clone().as_str())
+        .expect(format!("socket_url {:?} is not a valid URL", args.socket_url.clone()).as_str());
+    let password_bufread: BufRead = args.password
+        .as_bytes()
+        .into();
+
+    let _lair_client = ipc_keystore_connect(socket_url_parsed, password_bufread)
+        .await
+        .map_err(|e| format!("Failed to connect to lair keystore {:?}", e))
+        .unwrap();
+
+    println!("Successfully connected to lair keystore at {:?}", args.socket_url);
+}
